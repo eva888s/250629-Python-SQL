5cfa9215dff6465a43b26ed33a1bbd3f04c4e052
evashen <crazybuy888@gmail.com>
2025-07-27 08:04:29 +0000

    lesson8.sql

--- lesson8/lesson8_1.sql
+++ lesson8/lesson8_1.sql
@@ -1,6 +1,36 @@
-SELECT "stationCode",name,"stationAddrTw",日期,進站人數,出站人數
-FROM "台鐵車站資訊" LEFT JOIN "每日各站進出站人數" ON "stationCode" = "車站代碼"
+CREATE TABLE basket_a(
+	a INT PRIMARY KEY,
+	fruit_a VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE basket_b(
+	b INT PRIMARY KEY,
+	fruit_b VARCHAR(100) NOT NULL
+);
+
+INSERT INTO basket_a (a, fruit_a)
+VALUES
+    (1, 'Apple'),
+    (2, 'Orange'),
+    (3, 'Banana'),
+    (4, 'Cucumber');
+
+INSERT INTO basket_b (b, fruit_b)
+VALUES
+    (1, 'Orange'),
+    (2, 'Apple'),
+    (3, 'Watermelon'),
+    (4, 'Pear');
 
 
-SELECT COUNT(*) AS "筆數"
-FROM "台鐵車站資訊" LEFT JOIN "每日各站進出站人數" ON "stationCode" = "車站代碼"
+SELECT a, fruit_a, b, fruit_b
+FROM basket_a LEFT JOIN basket_b ON fruit_a = fruit_b
+
+SELECT a, fruit_a, b, fruit_b
+FROM basket_a RIGHT JOIN basket_b ON fruit_a = fruit_b
+
+SELECT a,fruit_a,b,fruit_b
+FROM basket_a INNER JOIN basket_b ON fruit_a = fruit_b
+
+SELECT a, fruit_a, b, fruit_b
+FROM basket_a FULL OUTER JOIN basket_b ON fruit_a = fruit_b
